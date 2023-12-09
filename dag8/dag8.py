a3d41fbe2f7d131c65c630d892a7b271874392a2
Børge Bjørnstadjordet <borgebj@uio.no>
2023-12-09 01:32:13 +0100

    Day 8 complete

--- dag8/dag8.py
+++ dag8/dag8.py
@@ -1,5 +1,9 @@
+import math
+
 with open("input.txt", "r") as f:
-    data = [line.strip() for line in f if line.strip()]
+    read = f.read()
+    data = [line.strip() for line in read.splitlines() if line]  # gets lines from file
+    startNodes = [line.strip() for line in read.split() if line and line.endswith("A")]  # nodes ending with A
 
 
 def parse_to_map(data):
@@ -8,50 +12,51 @@ def parse_to_map(data):
         content = e.replace(" ", "").split("=")
         key = content[0]
         val = tuple(map(str.strip, content[1][1:-1].split(',')))
-
         mapping[key] = val
 
     return mapping
 
 
-def traverse(map_data, instructions):
-    original_instructions = instructions.copy()
-    start = list(map_data.keys())[0]
-    end = "ZZZ"
-
-    steps = 0
-    while start != end:
-        if start not in map_data: raise ValueError(f"'{start}' is not a valid key in map_data")
-        if not instructions:
-            instructions = original_instructions.copy()
-
-        instruction = instructions.pop(0)
-
-        dir_index = 0 if instruction == "L" else 1
-        to = map_data[start][dir_index]
-
-        start = to
-        steps += 1
-
-    return steps
-
-
-def traverse_2(map_data, pos, steps, ins, orig):
-
+# doesn't work but should be the same
+def traverse_rec(map_data, pos, steps, dirs):
     if pos == "ZZZ":
         return steps
 
-    if not ins:
-        ins = orig.copy()
-        print(ins)
-        print(orig)
+    direction = dirs[steps % len(dirs)]
+    dir_indx = 0 if direction == "L" else 1
 
-    direction = 0 if ins.pop(0) == "L" else 1
-    to = map_data[pos][direction]
-    return traverse_2(map_data, to, steps+1, ins, orig)
+    to = map_data[pos][dir_indx]
+    return traverse_rec(map_data, to, steps + 1, dirs)
 
 
-instructions = [x for x in data.pop(0)]
+# traverses iteratively, works
+def traverse_it(map_data, pos, steps, directions):
+    while pos != "ZZZ":
+        direction = directions[steps % len(directions)]
+        pos = map_data[pos][0] if direction == "L" else map_data[pos][1]
+        steps += 1
+    return steps
+
+
+# traverses from nodes ending with Z to nodes ending with Z (can be multiple)
+def traverse_mult(map_data, starts, directions):
+    path_steps = [0] * len(starts)
+    dir_length = len(directions)
+
+    # gathers steps for each start-node
+    for i, node in enumerate(starts):
+        while not node.endswith("Z"):
+            direction = directions[path_steps[i] % dir_length]
+            node = map_data[node][0] if direction == "L" else map_data[node][1]
+            path_steps[i] += 1
+
+    return math.lcm(*path_steps) # calculates the LCM of steps in the list
+
+
+directions = [x for x in data.pop(0)]
 map_data = parse_to_map(data)
-steps = traverse_2(map_data, list(map_data.keys())[0], 0, instructions.copy(), instructions.copy())
-print(f'Steps or whatever {steps}')
+steps = traverse_it(map_data, "AAA", 0, directions)
+mult_steps = traverse_mult(map_data, startNodes, directions)
+
+print(f'Steps from AAA nodes to ZZZ nodes: {steps}')
+print(f'Steps ending with A to ending with Z: {mult_steps}')
